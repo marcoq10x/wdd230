c86cb1828e0af1d92b2264c0f52764cd2fb987d0
marcoq10x <marcoq10x@gmail.com>
2023-09-17 00:05:32 -0400

    Assignment Portal

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Populate copyright year
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = currentYear;
+
+    // Populate last modified date
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = "Last Updated: " + lastModified;
+});
+
+
+
