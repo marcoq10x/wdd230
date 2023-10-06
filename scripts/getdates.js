8e2d62389d190e1d38d1167ab8464e7b9e8e368f
marcoq10x <marcoq10x@gmail.com>
2023-10-06 00:36:56 -0400

    home page week 3

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,8 +5,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Populate last modified date
     const lastModified = document.lastModified;
-    document.getElementById("lastModified").textContent = "Last Updated: " + lastModified;
+    document.getElementById("lastModified").textContent = lastModified;
 });
 
-
-
+function toggleMenu() {
+    const navItems = document.getElementById("mainNav");
+    if (navItems.style.display === "none" || navItems.style.display === "") {
+        navItems.style.display = "flex";
+        document.getElementById("hamburger").innerText = 'X';  // Change to 'X' for close
+    } else {
+        navItems.style.display = "none";
+        document.getElementById("hamburger").innerText = '≡';  // Change back to '≡'
+    }
+}
