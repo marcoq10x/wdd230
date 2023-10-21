13e7f45987c613735459b7af812a026aab906e2f
marcoq10x <marcoq10x@gmail.com>
2023-10-20 23:48:19 -0400

    chamber of commerce home

---
+++ chamber/scripts/lastmod.js
@@ -0,0 +1,41 @@
+// Last Modification Function
+document.addEventListener("DOMContentLoaded", function() {
+    // Populate copyright year
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = currentYear;
+
+    // Populate last modified date
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = lastModified;
+});
+
+
+// Hero Image Slideshow Function
+let currentImageIndex = 0;
+const heroImages = [
+    "/wdd230/chamber/images/clock.jpg",
+    "/wdd230/chamber/images/umbrella.jpg",
+    "/wdd230/chamber/images/ocean.jpg",
+    "/wdd230/chamber/images/terrace.jpg"
+];
+
+function changeHeroImage() {
+    const hero = document.querySelector('.hero');
+    if (hero) {
+        hero.style.backgroundImage = `url(${heroImages[currentImageIndex]})`;
+        currentImageIndex = (currentImageIndex + 1) % heroImages.length;
+    }
+}
+
+setInterval(changeHeroImage, 3000); // Change image every 3 seconds
+
+
+
+// Hamburger Menu Toggle Function
+const hamburger = document.querySelector('#hamburger');
+const navigation = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamburger.classList.toggle('open');
+});
