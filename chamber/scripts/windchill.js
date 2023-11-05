3d74c760345f05e1e2c07ab503b26f86162a9a79
marcoq10x <marcoq10x@gmail.com>
2023-11-05 00:39:46 -0400

    week 7

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,13 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const temperature = parseFloat(document.getElementById('temperature').textContent);
-    const windSpeed = parseFloat(document.getElementById('windSpeed').textContent);
-
-    let windChill = 'N/A';
-
-    if (temperature <= 50 && windSpeed > 3) {
-        windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
-        windChill = windChill.toFixed(2) + '°F';
+//Wind chill function (Chamber of Commerce)
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 50 && windSpeed > 3.0) {
+      return 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+    } else {
+      return "N/A";
     }
-
-    document.getElementById('windChill').textContent = windChill;
-});
+  }
+  
+  function displayWindChill() {
+    const tempSpan = document.getElementById('temp-value');
+    const windSpeedSpan = document.getElementById('wind-speed-value');
+    const windChillSpan = document.getElementById('wind-chill-value');
+    
+    const temperature = parseFloat(tempSpan.textContent);
+    const windSpeed = parseFloat(windSpeedSpan.textContent);
+    
+    const windChill = calculateWindChill(temperature, windSpeed);
+    
+    windChillSpan.textContent = typeof windChill === "number" ? windChill.toFixed(2) + "°F" : windChill;
+  }
+  
+  //displayWindChill function when the script loads
+  displayWindChill();
+  
