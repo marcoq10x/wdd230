bbfa0caeb6a0673c463adb08fcae622c30768f15
marcoq10x <marcoq10x@gmail.com>
2023-11-30 01:34:25 -0500

    JSON menu 3

--- scripts/links.js
+++ scripts/links.js
@@ -16,8 +16,34 @@ async function getLinks() {
   }
 }
 
+// function displayLinks(lessons) {
+//   const navList = document.querySelector('.lesson-links'); //<ul class="lesson-links"></ul> in HTML where  links will appear
+
+//   navList.innerHTML = ''; // Clears any existing content in the list
+
+
+//   lessons.forEach(lesson => {
+//     let listItem = document.createElement('li');
+//     listItem.innerHTML = `<strong>Lesson ${lesson.lesson}</strong>`;
+//     let sublist = document.createElement('ul');
+
+//     lesson.links.forEach(link => {
+//       let subListItem = document.createElement('li');
+//       subListItem.innerHTML = `<a href="${baseURL}${link.url}">${link.title}</a>`;
+//       sublist.appendChild(subListItem);
+//     });
+
+//     listItem.appendChild(sublist);
+//     navList.appendChild(listItem);
+//   });
+// }
+
+// getLinks();
+
+
 function displayLinks(lessons) {
-  const navList = document.querySelector('.lesson-links'); //<ul class="lesson-links"></ul> in HTML where  links will appear
+  const navList = document.querySelector('.lesson-links');
+  navList.innerHTML = ''; // Clears any existing content in the list
 
   lessons.forEach(lesson => {
     let listItem = document.createElement('li');
@@ -26,7 +52,10 @@ function displayLinks(lessons) {
 
     lesson.links.forEach(link => {
       let subListItem = document.createElement('li');
-      subListItem.innerHTML = `<a href="${baseURL}${link.url}">${link.title}</a>`;
+      // Check if the link is an absolute URL
+      const href = link.url.startsWith('http') ? link.url : `${baseURL}${link.url}`;
+      console.log('Link HREF:', href); // Add this line to log the href
+      subListItem.innerHTML = `<a href="${href}">${link.title}</a>`;
       sublist.appendChild(subListItem);
     });
 
