3ef2b6d767716d8e0c36e490c1df8251ca30a2a5
marcoq10x <marcoq10x@gmail.com>
2023-12-04 23:15:38 -0500

    Home page wk 10

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -44,13 +44,52 @@ localStorage.setItem("numVisits-ls", numVisits);
 
 
 
+// // Select HTML elements in the document
+// const currentTemp = document.querySelector('#current-temp');
+// const weatherIcon = document.querySelector('#weather-icon');
+// const captionDesc = document.querySelector('figcaption');
+
+// // API URL
+// const url = 'https://api.openweathermap.org/data/2.5/weather?lat=26.67&lon=80.26&units=imperial&appid=0f4ae5904674a05d482b5ef14addb3e';
+
+// async function apiFetch() {
+//     try {
+//         const response = await fetch(url);
+//         if (response.ok) {
+//             const data = await response.json();
+//             console.log(data); // For testing
+//             displayResults(data);
+//         } else {
+//             throw Error(await response.text());
+//         }
+//     } catch (error) {
+//         console.error('Error fetching the data: ', error);
+//     }
+// }
+
+// apiFetch();
+
+// function displayResults(data) {
+//     currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
+//     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+//     let desc = data.weather[0].description;
+//     weatherIcon.setAttribute('src', iconsrc);
+//     weatherIcon.setAttribute('alt', desc);
+//     captionDesc.textContent = capitalize(desc);
+// }
+
+// function capitalize(str) {
+//     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+// }
+
+
 // Select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-// API URL
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=26.67&lon=80.26&units=imperial&appid=0f4ae5904674a05d482b5ef14addb3e';
+// URL for API request - Palm Beach, Florida
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=26.67&lon=80.26&units=imperial&appid=0f4ae5904674da05d482b5ef14addb3e';
 
 async function apiFetch() {
     try {
@@ -69,6 +108,7 @@ async function apiFetch() {
 
 apiFetch();
 
+// Function to display the results in HTML
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
@@ -78,6 +118,7 @@ function displayResults(data) {
     captionDesc.textContent = capitalize(desc);
 }
 
+// Helper function to capitalize each word in the description
 function capitalize(str) {
     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
